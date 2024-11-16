bb556c5fc14315f67b64a2f6909ce5c48dffac15
Aditya <jackuserme8881@gmail.com>
2024-11-17 00:20:07 +0530

    Integrated API
    Integrated Google gemini api

---
+++ src/context/Context.jsx
@@ -0,0 +1,25 @@
+import { createContext } from "react";
+import run from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+    
+    const onSent = async (prompt) => {
+        await run(prompt)
+    }
+
+    onSent("what is react js")
+
+    const contextValue = {
+        
+    }
+
+    return (
+        <ContextProvider value={contextValue}>
+            {props.children}
+        </ContextProvider>
+    )
+}
+
+export default ContextProvider
